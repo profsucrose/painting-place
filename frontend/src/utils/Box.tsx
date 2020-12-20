bcdb5690b5c914693f0b7fce06e697b6a544ed22
Tarek Ali <tarekali15@outlook.com>
2020-12-20 15:01:02 -0600

    Box.tsx; not done yet

---
+++ frontend/src/utils/Box.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+
+export default function Box(props: object) {
+  const mesh = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => {
+    mesh.current?.rotation?.x = mesh.current.rotation.y += 0.01;
+  });
+}
