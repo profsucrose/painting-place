c71dae934cf602aabd01479217d1eabaa56c36e7
Tarek Ali <tarekali15@outlook.com>
2020-12-20 15:12:50 -0600

    its call coming together

--- frontend/src/components/Box.tsx
+++ frontend/src/components/Box.tsx
@@ -1,15 +1,41 @@
 import ReactDOM from "react-dom";
 import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
-import * as THREE from 'three'
+import * as THREE from "three";
 
-export default function Box(props: object) 
-  const mesh = useRef();
+function Box(props: object) {
+  const mesh = useRef<THREE.Mesh>();
 
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
   useFrame(() => {
-    mesh.current?.rotation?.x = mesh.current.rotation.y += 0.01;
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
 }
+
+ReactDOM.render(
+  <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>,
+  document.getElementById("root")
+);
+
+export default Box;
